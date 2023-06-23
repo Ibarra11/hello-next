f61be4f6e0f4dcf0b8f0ebe656508e2cc46de575
Alan Ibarra <alanjibarradev@gmail.com>
2023-06-23 13:57:11 -0700

    change color

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <main>
-      <h1>Hello Alan Ibarra</h1>
+      <h1 style={{ color: "red" }}>Hello Alan Ibarra</h1>
     </main>
   );
 }
